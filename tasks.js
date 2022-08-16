579d84c8864445351e6d2f108e6a5ce8eafa7ca2
Denis <brokenhandq3@yandex.ru>
2022-08-16 23:41:37 +0300

    task-some-tasks

--- tasks.js
+++ tasks.js
@@ -29,3 +29,51 @@ function squareDigits(num){
   var str = String(num).split('').reduce((acc, el) => acc + Number(el) * Number(el), '')
   return parseInt(str)
 }
+
+// Remove anchor from URL
+function removeUrlAnchor(url){
+  // TODO: complete
+  var isHash = url.includes('#');
+  if(isHash) {
+    return url.slice(0, url.indexOf('#'))
+  }
+  else return url
+}
+
+//First non-repeating character
+
+function firstNonRepeatingLetter(s) {
+  // Add your code here
+  var obj = {};
+  
+  if(s.length === 1) {
+    return s;
+  }else if(!s) {
+    return '';
+  }
+
+    s.toLowerCase().split('').forEach(el => {
+        if(el in obj) {
+            obj[el] += 1;
+        } else {
+            obj[el] = 0;
+        }
+    })
+  
+  if(Object.keys(obj).length === 1) {
+    return ''
+  }
+  
+  var repeats = Object.values(obj);
+  if(repeats.every(el => el === 1)) {
+    return ""
+  }
+  
+  for(var key in obj) {
+    if(obj[key] === 0) {
+        var idx =  s[s.toLowerCase().indexOf(key)]
+        console.log(idx)
+        return idx
+    } 
+  }
+}
