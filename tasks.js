f7faf79a9c80734562026977e93b536dd1c01f3e
Denis <brokenhandq3@yandex.ru>
2022-08-17 00:05:13 +0300

    task-whoLikesIt

--- tasks.js
+++ tasks.js
@@ -99,3 +99,13 @@ function findEvenIndex(arr) {
   var sum = (arr) => arr.reduce((acc, el) => acc + el, 0)
   return arr.findIndex((_, i) => sum(arr.slice(0, i)) === sum(arr.slice(i + 1)))
 }
+
+// Who likes it?
+function likes(names) {
+  // TODO
+  if(names.length === 0) return 'no one likes this'
+  if(names.length === 1) return names[0] + ' likes this' 
+   if(names.length === 2) return names[0] +' and '+ names[1]+ ' like this' 
+  if(names.length === 3)return  `${names[0]}, ${names[1]} and ${names[2]} like this`
+  return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`
+}
