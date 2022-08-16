47e3139f35a05613b0a4ddbf8ae532053ad06c9f
Denis <brokenhandq3@yandex.ru>
2022-08-16 23:32:25 +0300

    task-sortByLength

--- tasks.js
+++ tasks.js
@@ -7,3 +7,18 @@ function factorial(n) {
   return n * factorial(n - 1) 
 } 
 
+//Sort array by string length
+function sortByLength (arr) {
+  // Return an array containing the same strings, ordered from shortest to longest
+  //return array.sort((a, b) => a.length - b.length);
+    for(let i = 0; i < arr.length; i++) {
+      for(let j = 0; j < arr.length-1; j++) {
+        if(arr[j].length > arr[j + 1].length) {
+            var temp = arr[j];
+            arr[j] = arr[j + 1]
+            arr[j + 1] = temp;
+        }
+      }
+    }
+  return arr
+};
