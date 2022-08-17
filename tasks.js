d4978ae9e9514a06d2618b35c4b8da7a7b0b3735
Denis <brokenhandq3@yandex.ru>
2022-08-18 00:02:14 +0300

    task-appleTurnover

--- tasks.js
+++ tasks.js
@@ -194,3 +194,9 @@ function factorial(n) {
     }
     return fc;
 }
+
+// Alan Partridge II - Apple Turnover
+function apple(x){
+  if(Math.pow(x, 2) > 1000) return `It's hotter than the sun!!`
+  return `Help yourself to a honeycomb Yorkie for the glovebox.`
+}
