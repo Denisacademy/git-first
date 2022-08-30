6c85968f3917ef34cc3f7a060558a99853b4a0af
Denis <brokenhandq3@yandex.ru>
2022-08-30 17:27:59 +0300

    add reverser

--- structure-data.js
+++ structure-data.js
@@ -1,20 +1,26 @@
 class Arr {
   len = 0;
+  
   constructor() {
     this.arr = [];
     this.arr = [];
-    this.pushOne('first')
+    this.pushOne('first');
     this.unshiftOne('begin');
     this.insertOne('middle', 1);
+    this.pushOne('CSS');
+    this.pushOne('JS');
+    this.insertSome(4, 'HTML', 'Node.js', 'Typescript');
   }
-  
-  pushOne(val) { //add end
+
+  pushOne(val) {
+    //add end
     this.arr[this.len] = val;
     this.len++;
     return this.arr;
   }
 
-  unshiftOne(val) { //add begin
+  unshiftOne(val) {
+    //add begin
     const cut = this.arr.slice();
     this.arr = [val].concat(cut);
     this.len++;
@@ -29,14 +35,12 @@ class Arr {
 
   popOne() {
     this.arr = this.arr.slice(0, this.len - 1);
-    
     this.len--;
     return this.arr;
   }
-  
-  
+
   deleteOne(pos) {
-    if(!this.arr.length) throw Error('nothing to delete')
+    if (!this.arr.length) throw Error('nothing to delete');
     const start = this.arr.slice(0, pos);
     const end = this.arr.slice(pos + 1);
     this.arr = start.concat(end);
@@ -44,10 +48,10 @@ class Arr {
     return this.arr;
   }
 
-  deleteFromTo(start, end) { //1 , 3
-    if(!this.len) throw Error('nothing to delete')
-    if(end > this.len - 1 || !start && !end) throw Error('no way')
-
+  deleteFromTo(start, end) {
+    //1 , 3
+    if (!this.len) throw Error('nothing to delete');
+    if (end > this.len - 1 || (!start && !end)) throw Error('no way');
     const begin = this.arr.slice(0, start);
     const finish = this.arr.slice(end + 1);
     this.arr = begin.concat(finish);
@@ -55,10 +59,10 @@ class Arr {
   }
 
   insertOne(val, pos) {
-    if(this.len <= 1) throw Error('operation is cancelled')
-      
+    if (this.len <= 1) throw Error('operation is cancelled');
+
     //if(pos === 0) return this.unshiftEl(val);
-      
+
     const ins = [val];
     const start = this.arr.slice(0, pos);
     const end = this.arr.slice(pos);
@@ -68,7 +72,7 @@ class Arr {
   }
 
   insertSome(pos, ...val) {
-    if(this.len <= 1 || pos > this.len) throw Error('operation is cancelled')
+    if (this.len <= 1 || pos > this.len) throw Error('operation is cancelled');
     const start = this.arr.slice(0, pos);
     const end = this.arr.slice(pos);
     this.arr = start.concat(val).concat(end);
@@ -76,9 +80,21 @@ class Arr {
     return this.arr;
   }
 
+  reverse() {
+    const revArr = [];
+    let count = 0;
+
+    while (count < this.len) {
+      revArr[count] = this.arr[this.len - 1 - count];
+      count++;
+    }
+
+    this.arr = revArr;
+    return this.arr;
+  }
+
   clearArr() {
     this.arr = [];
     this.len = 0;
   }
-  
 }
